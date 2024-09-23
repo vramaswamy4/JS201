942dd0a040f0f19f49841c48c0fe86321ee18f69
vramaswamy4 <vramaswamy4@wisc.edu>
2024-09-23 08:59:37 -0500

    complete project

---
+++ script.js
@@ -0,0 +1,46 @@
+document.getElementById("generateBtn").addEventListener("click", updateInformation);
+
+function updateInformation() {
+    let randInt =  Math.floor(Math.random() * 83);
+    fetch("https://swapi.dev/api/people/"+randInt)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            document.getElementById("name").innerText = data.name
+            document.getElementById("height").innerText = data.height
+            document.getElementById("mass").innerText = data.mass
+
+            fetch(data.homeworld)
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById("homeworld").innerText = data.name
+
+            })
+
+            document.getElementById("films").innerText = ""
+            for (let i = 0; i< data.films.length; i++) {
+                console.log(data.films[i])
+                fetch(data.films[i])
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById("films").innerHTML += `<li>${data.title}</li>`
+
+            })
+            }
+            // console.log(Array.isArray(data.films))
+            // document.getElementById("films").innerText = data.films
+            // document.getElementById("starships").innerText = data.starships
+
+            document.getElementById("starships").innerText = ""
+            for (let i = 0; i< data.starships.length; i++) {
+                console.log(data.starships[i])
+                fetch(data.starships[i])
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById("starships").innerHTML += + `<li>${data.name}</li>`
+
+            })
+            }
+            console.log("done with function")
+        })
+  }
